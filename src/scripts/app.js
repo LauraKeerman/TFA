6872c4bc45dbc0e9864c88dc2315da785c3ae575
Laura Keerman <laurakeerman@gmail.com>
2022-05-29 17:06:21 +0200

    gsap header

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,45 @@
 import gsap from 'gsap';
+
+import ScrollTrigger from 'gsap/ScrollTrigger'; //importer le gsap 
+gsap.registerPlugin(ScrollTrigger); //dire qu'on veut utiliser ce gsap
+
+
+
+if(window.matchMedia('(max-width: 767px)').matches){
+    const tl = gsap.timeline({ defaults: {duration: 3}});
+    tl.from('.h1-intro', {
+        x: -1000,
+        duration: 0.7,
+        stagger: 0.5,
+    });
+
+    tl.from('.p-intro', {
+        x: -1000,
+        duration: 0.6,
+    }, "-=0.3"); 
+
+    tl.from('.img-intro', {
+        x: -1000,
+        duration: 0.6,
+    }, "-=0.3"); 
+}
+
+
+if(window.matchMedia('(min-width: 768px)').matches){
+    const tl = gsap.timeline({ defaults: {duration: 3}});
+    tl.from('.h1-intro', {
+        x: -1000,
+        duration: 0.7,
+        stagger: 0.5,
+    });
+
+    tl.from('.p-intro', {
+        x: -1000,
+        duration: 0.6,
+    }, "-=0.3"); 
+
+    tl.from('.img-intro', {
+        x: 1000,
+        duration: 0.6,
+    }, "="); 
+}
