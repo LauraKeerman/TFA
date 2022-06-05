3a6ff3c5578b761cecc6a284e0623db3b5610c03
Laura Keerman <laurakeerman@gmail.com>
2022-06-05 18:00:08 +0200

    case study ok

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,58 +1,9 @@
-"use strict";
-
 //Copyright
 let date = new Date();
 let year = date.getFullYear();
 
 let yr = document.querySelector(".copyright");
 yr.innerText = year;
-
-
-import gsap from 'gsap';
-
-import ScrollTrigger from 'gsap/ScrollTrigger'; //importer le gsap 
-gsap.registerPlugin(ScrollTrigger); //dire qu'on veut utiliser ce gsap
-
-
-
-if(window.matchMedia('(max-width: 767px)').matches){
-    const tl = gsap.timeline({ defaults: {duration: 3}});
-    tl.from('.h1-intro', {
-        x: -1000,
-        duration: 0.7,
-        stagger: 0.5,
-    });
-
-    tl.from('.p-intro', {
-        x: -1000,
-        duration: 0.6,
-    }, "-=0.3"); 
-
-    tl.from('.img-intro', {
-        x: -1000,
-        duration: 0.6,
-    }, "-=0.3"); 
-}
-
-
-if(window.matchMedia('(min-width: 768px)').matches){
-    const tl2 = gsap.timeline({ defaults: {duration: 3}});
-    tl.from('.h1-intro', {
-        x: -1000,
-        duration: 0.7,
-        stagger: 0.5,
-    });
-
-    tl.from('.p-intro', {
-        x: -1000,
-        duration: 0.6,
-    }, "-=0.3"); 
-
-    tl.from('.img-intro', {
-        x: 1000,
-        duration: 0.6,
-    }, "="); 
-}
 
 
 //Slider
@@ -73,7 +24,6 @@ if(nextButton){
         plusSlides(1);
     });
 }
-
 
 // Next/previous controls
 function plusSlides(n) {
@@ -101,4 +51,77 @@ function showSlides(n) {
     }
 
     slides[slideIndex-1].classList.remove("hide"); 
+}
+
+
+import ScrollTrigger from 'gsap/ScrollTrigger'; //importer le gsap 
+gsap.registerPlugin(ScrollTrigger); //dire qu'on veut utiliser ce gsap
+import gsap from 'gsap';
+
+/*ANIMATIONS*/
+//Timelines
+if(window.matchMedia('(max-width: 767px)').matches){
+    const tl = gsap.timeline({ defaults: {duration: 3}});
+    tl.from('.h1-intro', {
+        x: -1000,
+        duration: 0.7,
+        stagger: 0.5,
+    });
+
+    tl.from('.p-intro', {
+        x: -1000,
+        duration: 0.6,
+    }, "-=0.3"); 
+
+    tl.from('.img-intro', {
+        x: -1000,
+        duration: 0.6,
+    }, "-=0.3"); 
+}
+
+
+if(window.matchMedia('(min-width: 768px)').matches){
+    const tl2 = gsap.timeline({ defaults: {duration: 3}});
+    tl2.from('.h1-intro', {
+        x: -1000,
+        duration: 0.7,
+        stagger: 0.5,
+    });
+
+    tl2.from('.p-intro', {
+        x: -1000,
+        duration: 0.6,
+    }, "-=0.3"); 
+
+    tl2.from('.img-intro', {
+        x: 1000,
+        duration: 0.6,
+    }, "="); 
+
+
+    /*const tl3 = gsap.timeline({ defaults: {duration: 3}});
+    tl3.from('.callToAction-right', {
+        x: 1000,
+        duration: 0.7,
+        stagger: 0.5,
+    });
+
+
+    tl3.from('.callToAction-left', {
+        x: -1000,
+        duration: 0.6,
+    }, "=");*/
+}
+
+
+//ScrollTrigger
+if(window.matchMedia('(min-width: 768px)').matches){
+    gsap.fromTo(".callToAction-right", {
+        x: 1000,
+        scrollTrigger: {
+            trigger: ".calltoAction-right",
+            start: 'top bottom',
+            toggleActions: 'play none reverse none',
+        }
+    });
 }
